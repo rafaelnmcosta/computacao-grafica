2a887f9934e5a2aec375e7019838ce6d2aaf235d
rafaelnmcosta <rafaelnmcosta@outlook.com>
2026-03-10 20:31:44 -0300

    T1: canvas renderizando

--- t1/script.js
+++ t1/script.js
@@ -1,1 +1,11 @@
 console.log("Trabalho 1 - Computação Gráfica");
+
+const tela = document.getElementById("tela");
+const cursor = tela.getContext("2d");
+
+let x = 196;
+let y = 196;
+
+cursor.fillStyle = "black";
+cursor.fillRect(x, y, 8, 8);
+
