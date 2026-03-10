3692f6181bb4e8d8b067f6c1cc90010d4c04651e
rafaelnmcosta <rafaelnmcosta@outlook.com>
2026-03-10 20:53:35 -0300

    T1: cleanup e implementação finalizada

--- t1/script.js
+++ t1/script.js
@@ -5,39 +5,54 @@ const cursor = tela.getContext("2d");
 
 let telaAltura = tela.height;
 let telaLargura = tela.width;
+let tamanhoCursor = 8;
 
 let x = telaLargura/2;
 let y = telaAltura/2;
 
 cursor.fillStyle = "black";
-cursor.fillRect(x, y, 8, 8);
+cursor.fillRect(x, y, tamanhoCursor, tamanhoCursor);
 
 document.addEventListener("keydown", function(event){
 
+    if(event.key === "Escape"){
+        window.close();
+    }
+
     if(event.key == "x" || event.key == "X" || event.key == "5"){
-        console.log("limpa");
         cursor.fillStyle = "white";
         cursor.fillRect(0, 0, telaLargura, telaAltura);
     }
 
     if(event.key == "w" || event.key == "W" || event.key == "8"){
-        y -= 8;
-        console.log("cima");
+        y -= tamanhoCursor;
     }
     else if(event.key == "s" || event.key == "S" || event.key == "2"){
-        y += 8;
-        console.log("baixo");
+        y += tamanhoCursor;
     }
     else if(event.key == "a" || event.key == "A" || event.key == "4"){
-        x -= 8;
-        console.log("esquerda");
+        x -= tamanhoCursor;
     }
     else if(event.key == "d" || event.key == "D" || event.key == "6"){
-        x += 8;
-        console.log("direita");
+        x += tamanhoCursor;
     }
 
     cursor.fillStyle = "black";
-    cursor.fillRect(x, y, 8, 8);
+    cursor.fillRect(x, y, tamanhoCursor, tamanhoCursor);
+
+    // controle pro cursor nao sair da tela
+    if(x < 0){
+        x = 0;
+    }
+    else if(x > telaLargura - tamanhoCursor){
+        x = telaLargura - tamanhoCursor;
+    }
+
+    if(y < 0){
+        y = 0;
+    }
+    else if(y > telaAltura - tamanhoCursor){
+        y = telaAltura - tamanhoCursor;
+    }
 
 });
